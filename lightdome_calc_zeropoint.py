25c0f095ed992eb7555b2c660bc6d2cccf62d9e1
NicoleRodriguezCavero <ndr0713@westminstercollege.edu>
2018-07-09 12:23:16 -0600

    Modified Zeropoint Calculation file to do a liner fit

--- lightdome_calc_zeropoint.py
+++ lightdome_calc_zeropoint.py
@@ -10,13 +10,17 @@
 #
 # MODIFICATION HISTORY
 # 2018-07-02 JRK: Commented template file made.
+# 2018-07-05 NRC: Calculated airmass and created scatter plot.
 #
 #
 
 # Import necessary packages
+from startup_slclight import *
 from astropy.io import ascii
 from astropy.table import vstack
 import glob
+from math import pi
+from scipy import stats
 
 # Set directory. In the future, we'll use the next 4 lines of code
 # so that the user can specify the directory from the command line.
@@ -47,17 +51,71 @@ for f in files:
 #    that usually you see a formula for airmass as a function of zenith angle,
 #    and our tables have altitude. Also, trigonometric functions are likely
 #    expecting angles in radians.
-#
+numpy_alt=np.array(data['alt'])
+zenith_ang=(90-(numpy_alt))*(pi/180)
+cos_zenith=np.cos(zenith_ang)
+sec_zenith=1/(cos_zenith)
+airmass= sec_zenith
+data['airmass']= airmass
+airmass_threshold= np.where(airmass<=4)
+data=data[airmass_threshold]
+airmass= data['airmass']
+
+
+
 # 4) Plot: on horizontal axis, airmass. On vertical axis, the sum 
 #    of the catalog magnitude + instrumental magnitude.
-# 
+numpy_vmag=np.array(data['Vmag'])
+numpy_instrmag=np.array(data['instrmag'])
+magnitudes = (numpy_vmag + numpy_instrmag)
+data['magnitudes']= magnitudes
+plt.scatter(airmass,magnitudes, color='c', s=10)
+plt.xlabel('Airmass')
+plt.ylabel('Magnitudes')
+plt.title('Instrumental Zero Point')
+
+
 # 5) Use a built-in package to find the best-fit line: we need to know
 #    the parameters (slope, intercept) and their errors. 
-#    Look for how to do this using numpy or scipy.
-# 
+# 	We might still need code that takes all the nan values, if any.
+slope, intercept, r_value, p_value, std_err = stats.linregress(airmass,magnitudes)
+r_squared = r_value**2
+
+ 
 # 6) Add the best-fit line onto the plot.
-#
+# Why when our x range is till five it graphs till 4?
+x= np.array(range(0,5))
+y= slope*x+intercept
+
+
+plt.plot(x,y, color='k', label='y= %fx + %f\nR^2=%f' %(slope,intercept,r_squared))
+plt.scatter(airmass,magnitudes, color='c', s=10)
+plt.xlabel('Airmass')
+plt.ylabel('Magnitudes')
+plt.title('Instrumental Zero Point')
+plt.legend()
+plt.show()
+
+
 # 7) Save the plot as a .png file.
-#
+plt.plot(x,y, color='k', label='y= %fx + %f\nR^2=%f' %(slope,intercept,r_squared))
+plt.scatter(airmass,magnitudes, color='c', s=10)
+plt.xlabel('Airmass')
+plt.ylabel('Magnitudes')
+plt.title('Instrumental Zero Point')
+plt.legend()
+plt.savefig(dir+'zeropoint.png')
+
+
 # 8) Somehow save the fit results (slope, intercept) into a text file that can then be 
 #    added to git and read in with future python scripts.
+
+
+
+
+
+
+
+
+
+
